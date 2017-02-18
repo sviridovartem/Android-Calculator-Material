30414a79596b34734c2ed685ac11ee38c6097a4d
Sviridov <SvyrydovArtem@gmail.com>
2017-02-18 19:17:18 +0200

    Initial

---
+++ app/src/main/java/com/example/sviridov/coolcalk/CalcActivity.java
@@ -0,0 +1,13 @@
+package com.example.sviridov.coolcalk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalcActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
